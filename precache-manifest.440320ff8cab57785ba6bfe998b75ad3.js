528133b6d0235dfd43dd000904b4a7b280df6358
DomHeal <Dominic.Heal@europe.com>
2020-04-19 08:01:09 +0000

    Deploying to gh-pages from  @ f70a511a06bd0c05070f4ba32efc52dcdfdf9686 🚀

---
+++ precache-manifest.440320ff8cab57785ba6bfe998b75ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934aa7a09283128409e70a57ff1c4d24",
+    "url": "/aoe2-lobby/index.html"
+  },
+  {
+    "revision": "dcc50a32d58a5a30719d",
+    "url": "/aoe2-lobby/static/css/main.451a0cbd.chunk.css"
+  },
+  {
+    "revision": "98759825b9825ad4c741",
+    "url": "/aoe2-lobby/static/js/2.4a9c0a51.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/aoe2-lobby/static/js/2.4a9c0a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcc50a32d58a5a30719d",
+    "url": "/aoe2-lobby/static/js/main.f0760e54.chunk.js"
+  },
+  {
+    "revision": "3d80b64881b892b1cb7b",
+    "url": "/aoe2-lobby/static/js/runtime-main.1fcc3058.js"
+  },
+  {
+    "revision": "7a3826f9159c2b325a3589a9b2af7142",
+    "url": "/aoe2-lobby/static/media/img1.7a3826f9.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aoe2-lobby/precache-manifest.bf789b5a179f3a63527b8b13a3fa8f21.js"
+  "/aoe2-lobby/precache-manifest.440320ff8cab57785ba6bfe998b75ad3.js"
 );
 
 self.addEventListener('message', (event) => {
